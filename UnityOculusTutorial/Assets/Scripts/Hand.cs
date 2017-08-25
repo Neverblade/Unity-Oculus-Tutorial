d1910e36de2467da2c178e89cb6b2edd2e424a6e
Ian McAulay <imcaulay@berkeley.edu>
2017-08-25 13:19:36 -0700

    Completed a rudimentary ball rolling game that uses Oculus. Follows similarly to the roll-a-ball tutorial.

---
+++ UnityOculusTutorial/Assets/Scripts/Hand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+    public Manager manager;
+
+    private float indexTriggerState;
+    private float oldIndexTriggerState;
+	
+	// Update is called once per frame
+	void Update () {
+        oldIndexTriggerState = indexTriggerState;
+        indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+
+        if (oldIndexTriggerState < 0.9f && indexTriggerState > 0.9f) {
+            Vector3 handPosition = OVRInput.GetLocalControllerPosition(controller);
+            manager.SpawnGame(gameObject, handPosition);
+        }
+	}
+}
---
+++ UnityOculusTutorial/Assets/Scripts/Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public float spawnScale;
+    public GameObject boardPrefab;
+    public GameObject ballPrefab;
+
+    private GameObject board = null;
+    private GameObject ball = null;
+
+    public void SpawnGame(GameObject hand, Vector3 position) {
+
+        // Delete board and ball if they exist.
+        if (board != null)
+            Destroy(board);
+        if (ball != null)
+            Destroy(ball);
+
+        // Spawn the board
+        board = Instantiate(boardPrefab, position, Quaternion.identity);
+        board.transform.localScale = Vector3.one * spawnScale;
+
+        // Spawn the ball
+        ball = Instantiate(ballPrefab, board.transform.Find("SpawnPoint").transform.position, Quaternion.identity);
+        ball.transform.localScale = Vector3.one * spawnScale;
+
+        // Parent the board under the hand.
+        board.transform.parent = hand.transform;
+    }
+}
