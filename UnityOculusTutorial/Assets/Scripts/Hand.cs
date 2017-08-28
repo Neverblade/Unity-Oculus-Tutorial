c1f63c9f280da6f519db3bd606a689081bec538b
Ian McAulay <imcaulay@berkeley.edu>
2017-08-28 12:30:04 -0700

    Work on 294

--- UnityOculusTutorial/Assets/Scripts/Hand.cs
+++ UnityOculusTutorial/Assets/Scripts/Hand.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class Hand : MonoBehaviour {
 
     public OVRInput.Controller controller;
-    public Manager manager;
+
+    public GameObject boardPrefab;
+    public GameObject ballPrefab;
+
+    private GameObject board;
+    private GameObject ball;
+
+    public float scale;
 
     private float indexTriggerState;
     private float oldIndexTriggerState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
         oldIndexTriggerState = indexTriggerState;
         indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
 
-        if (oldIndexTriggerState < 0.9f && indexTriggerState > 0.9f) {
-            Vector3 handPosition = OVRInput.GetLocalControllerPosition(controller);
-            manager.SpawnGame(gameObject, handPosition);
+        if (indexTriggerState > 0.9f && oldIndexTriggerState < 0.9f) {
+            Respawn();
         }
 	}
+
+    void Respawn() {
+        if (board != null) {
+            Destroy(board);
+            Destroy(ball);
+        }
+
+        Vector3 position = OVRInput.GetLocalControllerPosition(controller);
+
+        board = Instantiate(boardPrefab, position, Quaternion.identity);
+        board.transform.localScale = Vector3.one * scale;
+        board.transform.parent = transform;
+
+        ball = Instantiate(ballPrefab, position + new Vector3(0, 0.05f, 0), Quaternion.identity);
+        ball.transform.localScale = Vector3.one * scale;
+    }
 }
